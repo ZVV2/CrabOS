8e10847792df20f0be8baa366ec9a3dfaef702c9
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2025-05-12 12:10:03 +0200

    improved project structure

---
+++ crab_rpi/target/debug/deps/crabsy_rpi-049b72b2799b6130.d
@@ -0,0 +1,5 @@
+A:\projects\zvv2\CrabOS\crabsy_rpi\target\debug\deps\libcrabsy_rpi-049b72b2799b6130.rmeta: src\main.rs
+
+A:\projects\zvv2\CrabOS\crabsy_rpi\target\debug\deps\crabsy_rpi-049b72b2799b6130.d: src\main.rs
+
+src\main.rs:
