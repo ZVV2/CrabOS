543ad008aaac12915a544b791d5873ef9abebb99
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2025-05-18 11:37:37 +0200

    removing target folder and lock file

--- crab_rpi/target/debug/deps/crabsy_rpi-4892921e59843225.d
+++
@@ -1,5 +0,0 @@
-A:\projects\zvv2\CrabOS\crabsy_rpi\target\debug\deps\libcrabsy_rpi-4892921e59843225.rmeta: src\main.rs
-
-A:\projects\zvv2\CrabOS\crabsy_rpi\target\debug\deps\crabsy_rpi-4892921e59843225.d: src\main.rs
-
-src\main.rs:
